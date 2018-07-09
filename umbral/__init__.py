ae18250e9857dec6d62e2bb047c8d0c8d9a4eb16
Kieran Prasch <kieranprasch@gmail.com>
2018-07-08 17:45:23 -0700

    Add global metadata to umbral __init__

---
+++ umbral/__init__.py
@@ -0,0 +1,8 @@
+from umbral.__about__ import (
+    __author__,  __license__, __summary__, __title__, __version__
+)
+
+
+__all__ = [
+    "__title__", "__summary__", "__version__", "__author__", "__license__",
+]
