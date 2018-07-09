0e2f7da541384b22fa98a38cee98454dec34889d
Kieran Prasch <kieranprasch@gmail.com>
2018-07-09 14:06:24 -0700

    Reflect Pipfile in setup.py via distutils. NOTE: Broken subdeps.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from distutils.core import setup
 
+
 BASE_DIR = os.path.dirname(__file__)
 
 ABOUT = dict()
@@ -12,23 +13,33 @@ with open(os.path.join(BASE_DIR, "README.rst")) as f:
     long_description = f.read()
 
 
-TESTS_REQUIRE = [
-    'pytest',
-    'pytest-cov',
-    'pytest-mypy',
-    'pytest-mock',
-    'mock',
-    'coverage',
-    'codecov',
-    'monkeytype',
-    ]
-
-INSTALL_REQUIRES = ['msgpack-python',
-                    'pynacl',
+DEPENDENCY_LINKS = ["http://github.com/nucypher/bytestringSplitter/tarball/master#egg=byteStringSplitter-0.0.1",
+                    # "./wheelhouse/cryptography-2.3.dev1-cp35-cp35m-linux_x86_64.whl",
+                    # "./wheelhouse/cryptography-2.3.dev1-cp36-cp36m-linux_x86_64.whl",
+                    # "./wheelhouse/cryptography-2.3.dev1-cp37-cp37m-linux_x86_64.whl",
+                    # "./wheelhouse/cryptography-2.3.dev1-cp36-cp36m-macosx_10_13_x86_64.whl",
                     ]
 
-EXTRAS_REQUIRE = {'testing': TESTS_REQUIRE,
+INSTALL_REQUIRES = [
+                    # 'byteStringSplitter==0.0.1',
+                    'pynacl',
+                    'idna>=2.1',
+                    'asn1crypto>=0.21.0',
+                    'six>=1.4.1',
+                    'cffi>=1.7']
+
+EXTRAS_REQUIRE = {'testing': ['bumpversion',
+                              'pytest',
+                              'pytest-cov',
+                              'pytest-mypy',
+                              'pytest-mock',
+                              'mock',
+                              'coverage',
+                              'codecov',
+                              'monkeytype==18.2.0'],
+
                   'docs': ['sphinx', 'sphinx-autobuild'],
+
                   'benchmarks': ['pytest-benchmark'],
                   }
 
@@ -39,6 +50,7 @@ setup(name=ABOUT['__title__'],
       description=ABOUT['__summary__'],
       long_description=long_description,
 
+      dependency_links=DEPENDENCY_LINKS,
       extras_require=EXTRAS_REQUIRE,
       install_requires=INSTALL_REQUIRES,
       packages=['umbral'],
